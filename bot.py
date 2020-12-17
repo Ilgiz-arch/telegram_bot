63fc2862951e4e689b546f4225f4e9bcd0264561
Ilgiz.V <faizov@dev.picom.ru>
2020-12-17 22:08:44 +0300

    Добавлен запрос анекдотов

--- bot.py
+++ bot.py
@@ -1,16 +1,26 @@
 # Импортируем необходимые компоненты
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler
-from telegram.ext import Filters
+from  telegram import  ReplyKeyboardMarkup
+from telegram.ext import Updater,  CommandHandler, MessageHandler, Filters
 from settings import TG_TOKEN, TG_API_URL
-
+from bs4 import BeautifulSoup
+import requests
 # Функция sms() будет вызвана пользователем при отправке команды start
 # Внутри функции будет описана логика при ее вызове
 def sms(bot, update):
     print('Кто-то отправил команду /start. Что мне делать?') # вывод сообщения в консоль при отправки команды /start
-    bot.message.reply_text("Здравствуйте, я бот \nЯ пока не умею разговаривать, но я быстро учусь!".format(bot.message.chat.first_name)) # отправим ответ
-    print(bot.message)
+    my_keyboard = ReplyKeyboardMarkup([['Анекдот'], ['Начать']], resize_keyboard=True) # добавляем кнопку
+    bot.message.reply_text('Здравствуйте, {}! \nПоговорите со мной!'
+                           .format(bot.message.chat.first_name), reply_markup=my_keyboard)
+
+def get_anecdote(bot, update):
+    receive = requests.get('http://anekdotme.ru/random') # отправляем запрос к странице
+    page = BeautifulSoup(receive.text, 'html.parser') # подключаем html парсер, получаем текст страницы
+    find = page.select('.anekdot_text') # из страницы html  получаем class="anekdot_text"
+    for text in find:
+        page = (text.getText().strip()) # из class="anekdot_text" получаем текст и убираем пробелы по сторонам
+    bot.message.reply_text(page) # отправляем один анекдот, последний
+
+
 
 # Функция parrot() отвечает тем же сообщением , которое ему прислали
 def parrot(bot, update):
@@ -24,9 +34,9 @@ def main():
     my_bot = Updater(TG_TOKEN , TG_API_URL, use_context=True)
 
     my_bot.dispatcher.add_handler(CommandHandler('start', sms)) # обработчик команды start
-
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('Начать'), sms)) # обрабатываем текст кнопки
+    my_bot.dispatcher.add_handler(MessageHandler(Filters.regex('Анекдот'), get_anecdote)) # обрабатываем текст кнопки
     my_bot.dispatcher.add_handler(MessageHandler(Filters.text, parrot)) # обработчик текстового сообщения
-
     my_bot.start_polling() # проверяет о наличии сообщений с платформы Telegram
     my_bot.idle() # бот будет работать пока его не остановят
 
